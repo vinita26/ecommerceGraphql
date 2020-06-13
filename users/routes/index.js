bb39567abd7cdf713a4faf4485e43e3ee9f8b3b6
Arun Shankar Tiwari <arunstiwari@gmail.com>
2020-06-13 10:52:40 +0530

    implementation for userservice is done

--- users/routes/index.js
+++ users/routes/index.js
@@ -3,5 +3,8 @@ const {registerNewUser} = require('../controllers');
 const router = express.Router();
 
 router.post('/users', registerNewUser);
+router.post('/login', loginUser);
+router.get('/users', fetchAllUsers);
+router.get('/users/:id', fetchUserById);
 
 module.exports = router;
